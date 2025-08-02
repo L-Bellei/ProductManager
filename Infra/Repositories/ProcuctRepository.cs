449b2ca0a1b3cfb500f156fcd7592f6a23041d15
Leonardo Bellei <lbellei@vitait.com>
2025-08-02 19:55:06 -0300

    Added repository for manager products registries

---
+++ Infra/Repositories/ProcuctRepository.cs
@@ -0,0 +1,47 @@
+﻿using Microsoft.EntityFrameworkCore;
+using ProductManager.Domain.Entities;
+using ProductManager.Domain.Interfaces.Repositories;
+using ProductManager.Infra.Contexts;
+
+namespace ProductManager.Infra.Repositories;
+
+public class ProcuctRepository : IProductRepository
+{
+    private readonly ProductContext _context;
+    public ProcuctRepository(ProductContext context)
+    {
+        _context = context;
+    }
+
+    public async Task<Product> GetById(Guid id)
+    {
+        return await _context.Products.FindAsync(id) ?? throw new KeyNotFoundException("Product not found");
+    }
+
+    public async Task<IEnumerable<Product>> GetAll()
+    {
+        return await _context.Products.ToListAsync();
+    }
+
+    public async Task Add(Product product)
+    {
+        await _context.Products.AddAsync(product);
+    }
+
+    public async Task Update(Product product)
+    {
+        _context.Products.Update(product);
+    }
+
+    public async Task Delete(Guid id)
+    {
+        var product = await GetById(id);
+
+        _context.Products.Remove(product);
+    }
+
+    public async Task Commit()
+    {
+        await _context.SaveChangesAsync();
+    }
+}
